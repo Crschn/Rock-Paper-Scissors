25dc7dd8a3b3c04bd13b578e68fbe043e9c8b5cd
Crschn <clchung8@gmail.com>
2020-11-12 19:22:58 -0800

    first commit, just trying to remember what to do

---
+++ Constants.java
@@ -0,0 +1,13 @@
+/*I know that I overcomplicated it by using integers to be translated
+to words but I just did that to make it applicable to the object-oriented
+programming stuff we do that usually uses constants.*/
+
+public class Constants
+{
+    public static class Actions
+    {
+        public static int R = 1; //Rock
+        public static int P = 2; //Paper
+        public static int S = 3; //Scissors
+    }
+}
---
+++ README.txt
@@ -0,0 +1,2 @@
+Hello my name is Christian Chung and this is my little game I made
+It's called Rock Paper Scissors if you haven't heard about it
